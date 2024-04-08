bc892efa2b981fc42b92c0099a44867c01909e6b
KANNO <115959083+plutokann@users.noreply.github.com>
2024-04-08 18:38:56 +0200

    Add files via upload

---
+++ js/populate-prod.js
@@ -0,0 +1,24 @@
+fetch('../data/productions.json')
+  .then(response => response.json())
+  .then(data => {
+    const gridContainer = document.querySelector('#populate-productions');
+    // Clear the existing items
+    gridContainer.innerHTML = '';
+    
+    // Reverse the data array to iterate from newer to older
+    data.reverse();
+    
+    data.forEach(item => {
+      const div = document.createElement('div');
+      div.className = 'item';
+      div.innerHTML = `
+        <a href="#"><img src="${item.image}" alt=""></a>
+        <div>
+          <h3>${item.name}</h3>
+          <p>${item.medium}</p>
+          <p>${item.year}</p>
+        </div>
+      `;
+      gridContainer.appendChild(div);
+    });
+  });
